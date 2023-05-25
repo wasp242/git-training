6803b8967f7985660d48dd3e3186982a9c457533
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-05-25 17:43:06 +0400

    Complete the method which accepts an array of integers, and returns one of the following:

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const binaryArrayToNumber = (arr) => {
-  let arr1 = arr.reverse();
-  let sum = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    sum += arr1[i] * Math.pow(2, i);
-  }
-  return sum;
-};
+function isSortedAndHow(arr) {
+  const arrAsc = [...arr].sort((a, b) => a - b);
+  const arrDesc = [...arr].sort((a, b) => b - a);
 
-console.log(binaryArrayToNumber([0, 0, 0, 0, 0]));
+  const asc = arr.every((value, index) => value === arrAsc[index]);
+  const desc = arr.every((value, index) => value === arrDesc[index]);
+  if (asc) {
+    return "yes, ascending";
+  } else if (desc) {
+    return "yes, descending";
+  } else {
+    return "no";
+  }
+}
+
+console.log(isSortedAndHow([2, 42, 30]));
