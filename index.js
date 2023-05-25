0d74a6a96ab6857afd66492d8f125fd6b92596af
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-05-25 16:14:34 +0400

    Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-bugFix;
+function rowSumOddNumbers(n) {
+  let num = 1;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum = 0;
+    for (let j = 1; j <= i + 1; j++) {
+      if (i != 0) {
+        num = num + 2;
+        sum = sum + num;
+      } else {
+        sum = 1;
+      }
+    }
+  }
+  return sum;
+}
+
+console.log(rowSumOddNumbers(42));
