bb18c3a26f1d78d713c392fc4214c402442a43f4
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-05-30 16:55:03 +0400

    Tribonacci Sequence

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-function isSortedAndHow(arr) {
-  const arrAsc = [...arr].sort((a, b) => a - b);
-  const arrDesc = [...arr].sort((a, b) => b - a);
-
-  const asc = arr.every((value, index) => value === arrAsc[index]);
-  const desc = arr.every((value, index) => value === arrDesc[index]);
-  if (asc) {
-    return "yes, ascending";
-  } else if (desc) {
-    return "yes, descending";
-  } else {
-    return "no";
+function tribonacci(arr, n) {
+  for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = i; j < i + 3; j++) {
+      sum += arr[j];
+    }
+    arr.push(sum);
   }
+  return arr.slice(0, n);
 }
 
-console.log(isSortedAndHow([2, 42, 30]));
+console.log(tribonacci([1, 1, 1], 10));
